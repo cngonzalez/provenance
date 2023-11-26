89962329192b18d514378654af936aa31781ff04
Carolina Gonzalez <carolina.nicole.gonzalez@gmail.com>
2023-11-25 18:21:40 -0700

    cleanup app and add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  //lifecycle:
+  //choose paramour
+  //clear page
+  //choose fragment
+  //get longer part of fragment and choices of feeling
+  //get image based on feeling
+  //get next fragment option
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  return ( 
+
+    <div className="h-screen flex items-center justify-center">
+      <div className="font-serif prose">Components go here!</div>
+    </div>
   )
 }
 
